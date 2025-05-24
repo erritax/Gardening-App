24ae28e06735f47b87b0a9a7d6e62baf5e1dc5de
Errita Xu <117954430+erritax@users.noreply.github.com>
2025-05-24 16:05:13 -0400

    Update PlantJournal.py

--- PlantJournal.py
+++ PlantJournal.py
@@ -1,8 +1,3 @@
-# Errita Xu
-# May 30, 2023
-# FP App - Child Class
-# PlantJournal Screen: User can update gardening actions and jot down daily notes
-
 #import important libraries and the parent class
 from tkinter import *
 import tkinter as tk
